402320d36ab0cad89cbfbe82dc76b4d8b6cc814d
HabibAfzali <82744410+HabibullahAfzali@users.noreply.github.com>
2024-09-27 14:31:34 +0200

    Initionalization of the project structure

---
+++ src/main/java/com/example/explorcali_pa/ExplorcaliJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.explorcali_pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorcaliJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorcaliJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/explorcali_pa/ExplorcaliJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.explorcali_pa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorcaliJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
