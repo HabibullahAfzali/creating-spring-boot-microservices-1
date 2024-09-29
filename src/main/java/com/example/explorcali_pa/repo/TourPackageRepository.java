e40300a128d7197228afc47b24f6fe04fb5d946f
HabibAfzali <82744410+HabibullahAfzali@users.noreply.github.com>
2024-09-29 09:57:22 +0200

    Endpoint changed from tourpackages to packages in TourPackageRepository

--- src/main/java/com/example/explorcali_pa/repo/TourPackageRepository.java
+++ src/main/java/com/example/explorcali_pa/repo/TourPackageRepository.java
@@ -2,9 +2,11 @@ package com.example.explorcali_pa.repo;
 
 import com.example.explorcali_pa.model.TourPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(path = "packages",collectionResourceRel ="packages")
 public interface TourPackageRepository extends JpaRepository<TourPackage,String> {
 
 
